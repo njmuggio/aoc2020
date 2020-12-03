3cbfb89754402df65e189cc1add0f3ca23f1edb6
Nicholas Muggio <nicholasmuggio@gmail.com>
2020-12-03 05:53:59 -0700

    Added histogram to profiler

--- profile.sh
+++ profile.sh
@@ -35,3 +35,32 @@ done
 cmd+='))'
 s=$(bc -l <<< "$cmd")
 printf "SD:\t%f\n" $s
+
+min=${times[0]}
+max=${times[0]}
+for t in ${times[@]}
+do
+	if (($(bc <<< "$t < $min")))
+	then
+		min=$t
+	fi
+
+	if (($(bc <<< "$t > $max")))
+	then
+		max=$t
+	fi
+done
+
+bw=$(bc -l <<< "scale = 10; ($max - $min) / 10")
+
+col=${times[*]}
+tmp=$(mktemp)
+echo -e "${col// /\\n}" > $tmp
+gnuplot <<< "binwidth=$bw
+bin(x,width)=width*floor(x/width)
+set boxwidth binwidth
+set term dumb 120 31
+unset key
+plot '$tmp' using (bin(\$1,binwidth)):(1.0) smooth freq with boxes
+"
+rm $tmp
